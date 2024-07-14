eff8dced42b3fd94cb40ccd1f7b93c3851b505ed
yusufkaaneroglu <y.kaaneroglu@hotmail.com>
2024-07-14 21:40:13 +0300

    week1

--- list-methods-demo.py
+++ list-methods-demo.py
@@ -1,2 +1,51 @@
 names=["Ali","Yağmur","Hakan","Yusuf"]
 years= [1998,2000,1998,1987]
+
+#1)
+# names.append("Cenk")
+# print(names)
+#2)
+# names.insert(0, "Sena")
+# names.insert(len(names), "Mehmet")
+#3)
+# names.remove("Ali")
+# names.pop()
+# names.pop(1)
+#4)
+# index = names.index("Yusuf")
+# names.pop(index)
+# print(names)
+#5)
+# result="Ali" in names
+# result=names.index("Ali")
+# print(result)
+#6)
+# names.reverse()
+#7)
+# names.sort
+#8)
+# years.sort
+#9)
+# str= "Chevrolet,Dacia"
+# result=str.split(",")
+# print(result)
+#10)
+# min=min(years)
+# max=max(years)
+# print(min,max)
+#11)
+# result=years.count("1998")
+#12)
+# years.clear()
+#13)
+markalar = []
+marka = input("Marka: ")
+markalar.append(marka)
+
+marka = input("Marka: ")
+markalar.append(marka)
+
+marka = input("Marka: ")
+markalar.append(marka)
+
+print(markalar)
--- list-methods.py
+++ list-methods.py
@@ -1,9 +1,10 @@
-# numberlar ={1, 10, 5, 16, 4, 9, 10}
-# letters={"a", "g", "s", "b", "y", "a", "s"}
-# # val=min(numbers)
-# # val=max(numbers)
-# # val=max(letters)
-# # val=min(letters)
+numberlar ={1, 10, 5, 16, 4, 9, 10}
+letters={"a", "g", "s", "b", "y", "a", "s"}
 
-# val = numberlar[3:4]
-# print(val)
+# val=min(numbers)
+# val=max(numbers)
+# val=max(letters)
+# val=min(letters)
+
+val = numberlar[3:4]
+print(val)
